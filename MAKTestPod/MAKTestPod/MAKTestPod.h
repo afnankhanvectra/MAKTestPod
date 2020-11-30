0f5341bf06c6f58c1821eaf167e5e004f29486ff
afnan khan <afnan.khan@irsavideo.com>
2020-12-01 01:17:23 +0500

    App intialzer

---
+++ MAKTestPod/MAKTestPod/MAKTestPod.h
@@ -0,0 +1,18 @@
+//
+//  MAKTestPod.h
+//  MAKTestPod
+//
+//  Created by afnan khan on 01/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MAKTestPod.
+FOUNDATION_EXPORT double MAKTestPodVersionNumber;
+
+//! Project version string for MAKTestPod.
+FOUNDATION_EXPORT const unsigned char MAKTestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAKTestPod/PublicHeader.h>
+
+
